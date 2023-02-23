fc96c0e888bb44242b4c9f32ea8eed8e44271dcb
aman <77562702+amankhadse461@users.noreply.github.com>
2023-02-23 16:18:02 +0530

    commit

---
+++ Chapter 8 Set/Get the unique elements of a list.py
@@ -0,0 +1,4 @@
+fruits = ["mango","apple","pineapple","grapes"]
+print(fruits)
+unique_restaurants = set(fruits)
+print(unique_restaurants)
---
+++ Chapter 8 Set/Operations on sets.py
@@ -0,0 +1,11 @@
+x = {1,5,4,2,4,5,1,5,4,1,6,8,5,1}
+y = {5,4,5,4,1,5,7,1,2,8,1,2,7,1}
+
+#Union
+print("Union:", x|y)
+#Intersection
+print("Intersection:",x&y)
+#Difference
+print("Difference:",x-y)
+#Symmetric difference
+print("Symmetric difference:",x^y)
