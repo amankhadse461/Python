e01ec3d6c757cee33b1a45f2df8ff764532037b6
aman <77562702+amankhadse461@users.noreply.github.com>
2023-02-27 12:08:14 +0530

    commit

---
+++ Chapter 19 Dictionary/Dictionary.py
@@ -0,0 +1,19 @@
+
+#Dictionary
+marks ={
+    "ram":20,
+    "aman":50,
+    "sameer":50,
+    "harshal":52
+}
+print(marks)
+
+for key in marks:
+    print(key)
+
+#Iterating Over a Dictionary
+
+for key in marks:
+    print(key, marks[key])
+    
+ 
