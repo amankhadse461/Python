895772e97e2414c9dc7dbc23ecb3fa9376602c70
aman <77562702+amankhadse461@users.noreply.github.com>
2023-02-27 11:39:49 +0530

    commit

---
+++ Chapter 16 Loops/Loops in python.py
@@ -0,0 +1,36 @@
+## for loop
+
+#1
+fruits = ["Mango" ,"apple", "grapes", "pineapple","banana"]
+print(fruits)
+
+#2
+for i in fruits:
+    print(i)
+    
+fruits = {
+    "mango":"yellow",
+    "Apple":"red",
+    "banana":"yellow"
+}    
+
+print(fruits)
+for i in fruits:
+    print(i)
+    
+    
+##While Loop 
+
+#1
+i = 1
+while i<10:
+    print(i)
+    i = i+1   
+#2
+i = 1
+while i < 10:
+  print(i)
+  if i == 5:
+    break
+  i = i +1  
+    
