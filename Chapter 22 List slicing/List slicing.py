be7b1ab7bdcdb2a8b780841ed0f2a9597ae28777
aman <77562702+amankhadse461@users.noreply.github.com>
2023-02-27 17:41:46 +0530

    commit

---
+++ Chapter 22 List slicing/List slicing.py
@@ -0,0 +1,20 @@
+
+##Using the third "step" argument
+fruits = ["apple", "melon", "cherry", "custardapple", "kiwi", "dragonfruit"] 
+print(fruits)
+
+print(fruits[::2])
+
+print(fruits[::1])
+
+##Selecting a sublist from a list
+fruits = ["apple", "melon", "cherry", "custardapple", "kiwi", "dragonfruit"] 
+
+print(fruits[2:4])
+
+print(fruits[1:])
+
+##Reversing a list with slicing
+fruits = ["apple", "melon", "cherry", "custardapple", "kiwi", "dragonfruit"] 
+fruits.reverse()
+print(fruits)
