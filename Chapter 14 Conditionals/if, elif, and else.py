a40e243c518d416c33363bb4990a48401c04843d
aman <77562702+amankhadse461@users.noreply.github.com>
2023-02-27 11:11:52 +0530

    commit

---
+++ Chapter 14 Conditionals/if, elif, and else.py
@@ -0,0 +1,20 @@
+number = 5
+if number > 2:
+    print("Number is bigger than 2.")
+elif number < 2:  
+    print("Number is smaller than 2.")
+else: 
+    print("Number is 2.")
+    
+## If
+x = 50
+if x<100:
+    print("x is smalller:")
+
+   
+## if else
+x = 20
+if x>10:
+    print("x is greater:-")
+else:
+    print("x is smaller:-")  
