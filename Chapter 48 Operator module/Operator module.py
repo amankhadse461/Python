2dcf65ca26652cdfa6673b70c9bfe37fe3e0ca53
aman <77562702+amankhadse461@users.noreply.github.com>
2023-03-01 16:09:34 +0530

    commit

---
+++ Chapter 48 Operator module/Operator module.py
@@ -0,0 +1,11 @@
+import operator 
+print(operator.add(10,15),)
+
+from operator import * 
+print(add(10,20))
+print(mul(50,20))
+print(sub(50,20))
+print(truediv(50,20))
+print(floordiv(50,20))
+print(pow(50,20))
+print(eq(50,20))
