6147f8a29add82e77ced8e2976755e2e03ebc564
aman <77562702+amankhadse461@users.noreply.github.com>
2023-03-01 12:01:12 +0530

    commit

---
+++ Chapter 17 Arrays/Arrays.py
@@ -0,0 +1,32 @@
+fruits = ["mango","apple","grapes","bananan"]
+print(fruits)
+
+##Access individual elements through indexes
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+
+
+#append()
+print(fruits)
+fruits.append("pineapple")
+print(fruits)
+
+#Insert()
+fruits.insert(0,20)
+print(fruits)
+
+#remove()
+fruits.remove("apple")
+print(fruits)
+
+
+#pop()
+fruits.pop("pineapple")
+print(fruits)
+
+#reverse()
+fruits.reverse()
+print(fruits)
+
