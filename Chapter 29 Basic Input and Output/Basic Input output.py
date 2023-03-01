cd15ad89c1361fbdf51cdca6912a735549a82aae
aman <77562702+amankhadse461@users.noreply.github.com>
2023-03-01 13:49:54 +0530

    commit

---
+++ Chapter 29 Basic Input and Output/Basic Input output.py
@@ -0,0 +1,21 @@
+
+#Using the print function
+
+print("Hello My Name is aman")
+
+print("Have a great day")
+
+
+#Printing a string without a newline
+
+print("Hello, ", end="\n")
+print("World!")
+
+print("Hello, ", end="")
+print("World!")
+
+print("Hello, ", end="<br>")
+print("World!")
+
+print("Hello, ", end="BREAK")
+print("World!")
