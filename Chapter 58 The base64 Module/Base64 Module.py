32ef17a0c335e42e8aa5e024d9b4601c9e42e990
aman <77562702+amankhadse461@users.noreply.github.com>
2023-03-02 11:21:23 +0530

    commit

---
+++ Chapter 58 The base64 Module/Base64 Module.py
@@ -0,0 +1,33 @@
+import base64
+a  ="amankhadse"
+b  = a.encode("UTF-8")
+print(b)
+
+
+a  ="amankhadse"
+b  = a.encode("UTF-8")
+c = base64.b64encode(b)
+print(c)
+
+s1 = c.decode("UTF-8")
+print(s1)
+
+#Encoding and Decoding Base85
+import base64
+# Creating a string
+s = "hey i am aman!"
+
+b = s.encode("UTF-8")
+
+e = base64.b85encode(b)
+
+s1 = e.decode("UTF-8")
+
+print("Base85 Encoded:", s1)
+
+b1 = s1.encode("UTF-8")
+
+d = base64.b85decode(b1)
+
+s2 = d.decode("UTF-8")
+print(s2)
